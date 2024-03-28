f6515081c92b113d90f608f6125b4d490d58234a
shukhratovich-a <shukhratovich138@gmail.com>
2024-03-28 16:08:56 +0500

    refactor: connect homepage

--- src/component/AdminApp.tsx
+++ src/component/AdminApp.tsx
@@ -3,19 +3,30 @@ import { Admin, Resource } from "react-admin";
 import { dataProvider } from "@/dataProvider";
 import { authProvider } from "@/authProvider";
 
-import { ListTopic } from "./Topic/ListTopic";
-import { CreateTopic } from "./Topic/CreateTopic";
-import { EditTopic } from "./Topic/EditTopic";
-import { ShowTopic } from "./Topic/ShowTopic";
+import { ListSection } from "./Section/ListSection";
+import { CreateSection } from "./Section/CreateSection";
+import { EditSection } from "./Section/EditSection";
+import { ShowSection } from "./Section/ShowSection";
+
+import { ListHomepage } from "./Homepage/ListHomepage";
+import { EditHomepage } from "./Homepage/EditHomepage";
+import { ShowHomepage } from "./Homepage/ShowHomepage";
 
 const AdminApp = () => (
   <Admin dataProvider={dataProvider} authProvider={authProvider}>
     <Resource
-      name="topic"
-      list={ListTopic}
-      create={CreateTopic}
-      edit={EditTopic}
-      show={ShowTopic}
+      name="homepage"
+      edit={EditHomepage}
+      show={ShowHomepage}
+      list={ListHomepage}
+    />
+
+    <Resource
+      name="section"
+      list={ListSection}
+      create={CreateSection}
+      edit={EditSection}
+      show={ShowSection}
     />
   </Admin>
 );
