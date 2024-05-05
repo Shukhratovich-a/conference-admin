778dc9963df80d84aa1879f78d4bebbbf1a9b3a3
shukhratovich-a <shukhratovich138@gmail.com>
2024-05-05 05:47:23 +0500

    refactor: add poster upload

--- src/api/homepage.api.ts
+++ src/api/homepage.api.ts
@@ -2,7 +2,7 @@ import { UpdateParams } from "react-admin";
 
 import querystring from "query-string";
 
-import axios from "./axios";
+import axios, { fileUpload } from "./axios";
 
 export const homepage = {
   getList: async () => {
@@ -12,16 +12,25 @@ export const homepage = {
 
     const { data } = await axios.get(url);
 
+    data.poster = { src: data.poster };
+
     return { data: [data], total: 1 };
   },
 
   getOne: async () => {
     const { data } = await axios.get(`/homepage/get-with-contents`);
 
+    data.poster = { src: data.poster };
+
     return { data };
   },
 
   update: async ({ data: body }: UpdateParams) => {
+    if (body.poster && body.poster.rawFile) {
+      const { data: posterData } = await fileUpload(body.poster.rawFile);
+      body.poster = posterData.url;
+    } else delete body.poster;
+
     const { data } = await axios.put(`/homepage/update`, { ...body });
 
     return { data };
