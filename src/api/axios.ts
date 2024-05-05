2238226adf439ac38e62a4c9c1befda738bb4d87
shukhratovich-a <shukhratovich138@gmail.com>
2024-05-05 05:46:50 +0500

    refactor: add authorization to axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,19 +2,35 @@ import axios from "axios";
 
 export const DOMAIN = import.meta.env.VITE_SIMPLE_REST_URL;
 
-axios.defaults.baseURL = DOMAIN;
-
 interface IFile {
   url: string;
   name: string;
 }
 
+const fetchClient = () => {
+  const defaultOptions = {
+    baseURL: DOMAIN,
+    method: "get",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  let instance = axios.create(defaultOptions);
+
+  instance.interceptors.request.use(function (config) {
+    const token = localStorage.getItem("token");
+    config.headers.Authorization = token ? `Bearer ${token}` : "";
+    return config;
+  });
+
+  return instance;
+};
+
 export const fileUpload = async (file: File) => {
   const formData = new FormData();
   formData.append("file", file);
-  return axios.post<IFile>("/file/upload", formData, {
-    headers: { "Content-Type": "multipart/form-data" },
-  });
+  return fetchClient().post<IFile>("/file/upload", formData, { headers: { "Content-Type": "multipart/form-data" } });
 };
 
-export default axios;
+export default fetchClient();
