0c2d5054a3755cf1c62c60a7e296816a941b286b
shukhratovich-a <shukhratovich138@gmail.com>
2024-05-24 04:26:30 +0500

    refactor: add resources

--- src/component/AdminApp.tsx
+++ src/component/AdminApp.tsx
@@ -8,6 +8,18 @@ import { ListHeader } from "./Header/ListHeader";
 import { EditHeader } from "./Header/EditHeader";
 import { ShowHeader } from "./Header/ShowHeader";
 
+import { ListDate } from "./Date/ListDate";
+import { EditDate } from "./Date/EditDate";
+import { ShowDate } from "./Date/ShowDate";
+
+import { ListFee } from "./Fee/ListFee";
+import { EditFee } from "./Fee/EditFee";
+import { ShowFee } from "./Fee/ShowFee";
+
+import { ListSubmission } from "./Submission/ListSubmission";
+import { EditSubmission } from "./Submission/EditSubmission";
+import { ShowSubmission } from "./Submission/ShowSubmission";
+
 import { ListHomepage } from "./Homepage/ListHomepage";
 import { EditHomepage } from "./Homepage/EditHomepage";
 import { ShowHomepage } from "./Homepage/ShowHomepage";
@@ -32,6 +44,12 @@ const AdminApp = () => (
     <Resource name="header" list={ListHeader} edit={EditHeader} show={ShowHeader} />
 
     <Resource name="homepage" list={ListHomepage} edit={EditHomepage} show={ShowHomepage} />
+
+    <Resource name="date" list={ListDate} edit={EditDate} show={ShowDate} />
+
+    <Resource name="fee" list={ListFee} edit={EditFee} show={ShowFee} />
+
+    <Resource name="submission" list={ListSubmission} edit={EditSubmission} show={ShowSubmission} />
 
     <Resource name="user" list={ListUser} create={CreateUser} edit={EditUser} show={ShowUser} />
 
